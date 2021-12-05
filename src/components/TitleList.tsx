8762f5ff566bf82aa4797328442c9158f2dfd7ff
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-12-05 23:20:30 +0900

    タイトルページの実装

---
+++ src/components/TitleList.tsx
@@ -0,0 +1,64 @@
+import { VFC } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+import noImage from 'public/no_image.png';
+import { AddTitle } from './AddTitle';
+
+export type Title = {
+  id: number;
+  user_id: string;
+  title: string;
+  author: string;
+  image_url: string;
+};
+
+type TitlesProps = {
+  titles: Title[];
+  uuid: string;
+  getTitleList: VoidFunction;
+  filterText: string;
+};
+
+export const TitleList: VFC<TitlesProps> = (props) => {
+  const { titles, uuid, getTitleList, filterText } = props;
+
+  // 漫画タイトルのフィルタリング
+  // タイトルもしくは著者名に検索ワードが含まれている場合のみ表示
+  const filteredTitle = titles.filter((title) => {
+    let searchContent = title.title + ' ' + title.author;
+    return searchContent.toLowerCase().includes(filterText.toLowerCase());
+  });
+
+  return (
+    <div className='grid grid-cols-3 gap-2 m-4 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 2xl:grid-cols-8'>
+      <AddTitle uuid={uuid} getTitleList={getTitleList} />
+      {filteredTitle.map((title) => {
+        return (
+          <Link key={title.id} href={`/title?id=${title.id}`} passHref>
+            <div className='p-2 border cursor-pointer'>
+              <div className='flex justify-center'>
+                {title.image_url ? (
+                  <Image
+                    src={title.image_url}
+                    alt='thumbnail'
+                    width={126}
+                    height={200}
+                  />
+                ) : (
+                  <Image
+                    src={noImage}
+                    alt='thumbnail'
+                    width={126}
+                    height={200}
+                  />
+                )}
+              </div>
+              <div className='mt-2 text-center'>{title.title}</div>
+            </div>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,73 @@
 import type { NextPage } from 'next';
-import { ReactNode, VFC } from 'react';
+import { ReactNode, useCallback, useEffect, useState, VFC } from 'react';
 import { Auth, Button, IconLogOut } from '@supabase/ui';
 
 import { LayoutWrapper } from 'src/components/LayoutWrapper';
 // 作成したSupabeseクライアントをインポート
 import { client } from 'src/libs/supabase';
+import { Title, TitleList } from 'src/components/TitleList';
 
 type Props = {
   children: ReactNode;
+};
+
+// DBから漫画タイトルを取得
+const getTitles = async () => {
+  const { data, error } = await client
+    .from('manga_title')
+    .select('*')
+    .order('title');
+  if (!error && data) {
+    return data;
+  }
+  return [];
 };
 
 const Container: VFC<Props> = (props) => {
   const { children } = props;
   // ログイン状態を取得
   const { user } = Auth.useUser();
+  const [text, setText] = useState<string>('');
+  const [titles, setTitles] = useState<Title[]>([]);
 
-  // ログインしている場合 ログイン画面を表示
+  // DBから取得した漫画タイトルをセット
+  const getTitleList = useCallback(async () => {
+    const data = await getTitles();
+    setTitles(data);
+  }, []);
+
+  useEffect(() => {
+    getTitleList();
+  }, [user, getTitleList]);
+
+  // ログインしている場合
   if (user) {
     return (
-      <div className='flex justify-end mx-2 my-4'>
-        <Button
-          size='medium'
-          icon={<IconLogOut />}
-          onClick={() => client.auth.signOut()}
-        >
-          Sign out
-        </Button>
+      <div>
+        <div className='flex justify-center gap-2 p-4'>
+          <input
+            type='text'
+            className='w-full h-12 px-4 bg-white border border-gray-300 rounded shadow appearance-none hover:border-gray-500'
+            placeholder='Filtering text'
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+        </div>
+        <TitleList
+          titles={titles}
+          uuid={user.id}
+          getTitleList={getTitleList}
+          filterText={text}
+        />
+        <div className='flex justify-end mx-2 my-4'>
+          <Button
+            size='medium'
+            icon={<IconLogOut />}
+            onClick={() => client.auth.signOut()}
+          >
+            Sign out
+          </Button>
+        </div>
       </div>
     );
   }
